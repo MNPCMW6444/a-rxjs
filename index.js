ede61572012a86ab0053ea35264789bdc04bea79
Michael Nusair‬‏ <30693526+MNPCMW6444@users.noreply.github.com>
2022-09-01 16:40:29 +0300

    lhi

--- index.js
+++ index.js
@@ -1,26 +1,68 @@
 const Rxjs = require("rxjs");
 
 const a = new Rxjs.Observable((observer) => {
-  setTimeout(() => observer.next(1), 4000);
-  setTimeout(() => observer.next(2), 10000);
-  setTimeout(() => observer.next(3), 17000);
+  setTimeout(() => observer.next(1), 3000);
+  setTimeout(() => observer.next(2), 12000);
+  setTimeout(() => observer.next(3), 19000);
 });
 
 const b = new Rxjs.Observable((observer) => {
-  setTimeout(() => observer.next(0), 1000);
-  setTimeout(() => observer.next(2), 5000);
-  setTimeout(() => observer.next(4), 8000);
-  setTimeout(() => observer.next(5), 15000);
-  setTimeout(() => observer.next(7), 22000);
+  setTimeout(() => observer.next(0), 0);
+  setTimeout(() => observer.next(2), 4000);
+  setTimeout(() => observer.next(4), 7000);
+  setTimeout(() => observer.next(5), 14000);
+  setTimeout(() => observer.next(7), 21000);
 });
+const startTime = new Date().getSeconds();
+let relativeTime = new Date().getSeconds() - startTime;
+let diff, last;
+last = relativeTime;
 
-a.pipe(Rxjs.switchMap((x) => b.pipe(Rxjs.map((m) => x * m)))).subscribe(
-  console.log
-);
+a.pipe(
+  Rxjs.switchMap((x) =>
+    b.pipe(
+      Rxjs.map((m) => {
+        relativeTime = new Date().getSeconds() - startTime;
+        diff = relativeTime - last;
+        last = relativeTime;
+        return (
+          (relativeTime < 10 ? "0" + relativeTime : relativeTime) +
+          "(+" +
+          diff +
+          "):  x  *  m   =   " +
+          x +
+          "  *  " +
+          m +
+          "  =  " +
+          x * m
+        );
+      })
+    )
+  )
+).subscribe(console.log);
 
-/* a.pipe(Rxjs.mergeMap((x) => b.pipe(Rxjs.map((m) => x * m)))).subscribe(
-  console.log
-); */
+a.pipe(
+  Rxjs.mergeMap((x) =>
+    b.pipe(
+      Rxjs.map((m) => {
+        relativeTime = new Date().getSeconds() - startTime;
+        diff = relativeTime - last;
+        last = relativeTime;
+        return (
+          (relativeTime < 10 ? "0" + relativeTime : relativeTime) +
+          "(+" +
+          diff +
+          "):  x  *  m   =   " +
+          x +
+          "  *  " +
+          m +
+          "  =  " +
+          x * m
+        );
+      })
+    )
+  )
+).subscribe(console.log);
 
 //Rxjs.combineLatest(a, b).subscribe(console.log);
 
